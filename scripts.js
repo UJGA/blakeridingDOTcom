8e3edbf9796c91b90b146a2e1830886d1bf86729
Blake Riding <blakeriding@gmail.com>
2024-03-05 23:16:17 -0700

    created website

---
+++ scripts.js
@@ -0,0 +1,2 @@
+// You might not need any JS if you're only doing a scroll-based single-page site.
+// If you want to implement smooth scrolling or other interactive elements, you can add them here.
